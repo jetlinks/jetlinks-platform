7bbd3d4ddb0056d6e0c4a5cb5cf60e236fa100db
zhou-hao <admin@hsweb.me>
2019-10-28 16:58:01 +0800

    优化

--- src/main/java/org/jetlinks/platform/configuration/SpringProtocolSupports.java
+++ src/main/java/org/jetlinks/platform/configuration/SpringProtocolSupports.java
@@ -1,14 +1,14 @@
 package org.jetlinks.platform.configuration;
 
 import org.jetlinks.core.ProtocolSupport;
-import org.jetlinks.supports.DefaultProtocolSupports;
+import org.jetlinks.supports.protocol.StaticProtocolSupports;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class SpringProtocolSupports extends DefaultProtocolSupports implements BeanPostProcessor {
+public class SpringProtocolSupports extends StaticProtocolSupports implements BeanPostProcessor {
 
 
     @Override
