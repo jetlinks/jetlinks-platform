42d89235d442a6ad0df5ade40fac263c0152c372
gaoyf <admin@gaiys.com>
2019-10-31 14:19:52 +0800

    echarts

--- ui/plugins/tools/metadata-parser.js
+++ ui/plugins/tools/metadata-parser.js
@@ -6,8 +6,11 @@ define(["jquery"], function ($) {
         },
         "_default": function (value) {
             return "<span class=\"info-key\">属性值" + value + "在正常范围内</span>";
+        },
+        "_discount": function () {
+            return "<div class=\"info-key\" id=\"discount\"></div>";
         }
-    }
+    };
 
     var unifyUnit = {
         "celsiusDegrees": {
@@ -16,7 +19,50 @@ define(["jquery"], function ($) {
             "type": "temperature",
             "description": "温度单位:摄氏度(℃)",
             "getCharts": function (value) {
-                return valueTypeCharts["_default"](value+"℃");
+                var a = valueTypeCharts["_discount"]();
+                require(['echarts'], function (echarts) {
+                    var body = $("<div>").css("height", "20%");
+                    console.log(body);
+                    var c = body[0];
+                    $("#discount").html("").append(body);
+                    var myChart = echarts.init(c);
+                    // 指定图表的配置项和数据
+                    var option = {
+                        tooltip: {},
+                        xAxis: {
+                            axisLine: {show:false},//轴线不显示
+                            axisTick: {show:false},//
+                            show:false,
+                            data: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00"]
+                        },
+                        yAxis: {
+                            show:false,
+                            axisLine: {show:false},//轴线不显示
+                            axisTick: {show:false},//
+                            splitLine: {
+                                show: false //隐藏网格线
+                            }
+                        },
+                        grid: {
+                            top: '-1%',// 等价于 y: '16%'
+                            left: '-10%',
+                            right: '-10%',
+                            bottom: '-13%'
+                        },
+                        series: [{
+                            name: '温度',
+                            type: 'line',
+                            data: [5, 20, 40, 10, 10, 20, 5, 10, 20, 5],
+                            color: "#33CCFF",//颜色样式
+                            areaStyle: {},//折线下方阴影
+                            symbolSize: 2,   //设定实心点的大小
+                            center:["150%","50%"]
+                        }]
+                    };
+                    // 使用刚指定的配置项和数据显示图表。
+                    myChart.setOption(option);
+                });
+                return a;
             }
         },
         "percent": {
@@ -46,7 +92,7 @@ define(["jquery"], function ($) {
                 return valueTypeCharts["_default"](value);
             }
         }
-    }
+    };
 
     function normalProperty(property) {
         var resultMap = {};
@@ -60,7 +106,7 @@ define(["jquery"], function ($) {
                 property["valueType"].unifyUnit = unifyUnit["_undefine"];
             }
             return property["valueType"];
-        }
+        };
         return resultMap;
     }
 
@@ -70,10 +116,10 @@ define(["jquery"], function ($) {
         mapShallowCopy(func, resultMap);
         resultMap.getInputs = function () {
             return propertyParser(func.inputs);
-        }
+        };
         resultMap.getOutput = function () {
             return normalProperty(func.output);
-        }
+        };
         return resultMap;
     }
 
@@ -83,7 +129,7 @@ define(["jquery"], function ($) {
         mapShallowCopy(event, resultMap);
         resultMap.getParameters = function () {
             return propertyParser(event.parameters);
-        }
+        };
         return resultMap;
     }
 
@@ -98,7 +144,7 @@ define(["jquery"], function ($) {
         }
         resultMap.getProperty = function (id) {
             return resultMap[id];
-        }
+        };
         return resultMap;
     }
 
@@ -109,7 +155,7 @@ define(["jquery"], function ($) {
             target[key] = typeof source[key] === 'object' ? mapDeepCopy(source[key], target) : source[key];
         }
         return target;
-    }
+    };
     //浅拷贝
     var mapShallowCopy = function (source, target) {
         for (var key in source) {
@@ -118,7 +164,7 @@ define(["jquery"], function ($) {
             }
         }
         return target;
-    }
+    };
 
     function transObj(o) {
         if (typeof o === 'object') {
