9cc66d700772af22b9c748ef03b7097ec137d46d
wangzheng <540964791@qq.com>
2019-11-04 11:20:22 +0800

    增加权限设置初始化数据

--- src/main/resources/hsweb-starter.js
+++ src/main/resources/hsweb-starter.js
@@ -58,7 +58,7 @@ var menus = [{
     "sortIndex": 105,
     "status": 1,
     "url": "admin/user/list.html"
-},{
+}, {
     "icon": "fa fa-users",
     "id": "42fc4f83d12cc172e4690937eb15752a",
     "name": "维度管理",
@@ -68,7 +68,7 @@ var menus = [{
     "sortIndex": 103,
     "status": 1,
     "url": "admin/dimension/list.html"
-},{
+}, {
     "icon": "fa fa-fighter-jet",
     "id": "1191174249033035776",
     "name": "协议管理",
@@ -84,7 +84,7 @@ var menus = [{
     "name": "协议管理",
     "parentId": "1191174249033035776",
     "path": "eIah0-vXS7",
-    "permissionExpression": "resource:protocol",
+    "permissionExpression": "resource:protocol-supports",
     "sortIndex": 301,
     "status": 1,
     "url": "admin/protocol/list.html"
@@ -119,8 +119,103 @@ var menus = [{
     "status": 1,
     "url": "admin/device/instance/list.html"
 }];
-
-
+var autzSettings = [{
+    "permission":"protocol-supports",
+    "id":"638a9d26f31890b7d59544251bc638db",
+    "actions":["query","save","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"file",
+    "id":"41ef31347373c3020573b5975569de05",
+    "actions":["upload-static"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"user-token",
+    "id":"a76877d99938197151ba679af228977b",
+    "actions":["query","save"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"device-product",
+    "id":"f4754ac5a714ec97a4e5d6ef60083975",
+    "actions":["query","save","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"device-instance",
+    "id":"ef85383c8adb52fcbeb7b4fe6686c6c6",
+    "actions":["query","save","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"device-properties",
+    "id":"936f59a0ef4cabb17b5028f8339895d3",
+    "actions":["query","save","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"dictionary",
+    "id":"7dc2cb54ddd22053368c84d8ad8362f3",
+    "actions":["query","save","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"user",
+    "id":"5f59ccf52ea4c79f5eacc10cbf40d02",
+    "actions":["add","query","get","save","update","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"dimension",
+    "id":"89884beecc62035bc9b8d6e6b2b6a593",
+    "actions":["add","import","query","get","save","update","delete","export"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"permission",
+    "id":"31e57a1df89c14607758e3dbe618912a",
+    "actions":["add","query","get","save","update","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"menu",
+    "id":"7ebc7d1b4316ba444bc64ae6059cd201",
+    "actions":["add","query","get","save","update","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+},{
+    "permission":"autz-setting",
+    "id":"4e03a0db0a50c678f6b6e32cf9c31583",
+    "actions":["query","save","delete"],
+    "dimensionType":"user",
+    "dimensionTarget":"admin",
+    "dimensionTargetName":"超级管理员",
+    "dimensionTypeName":"用户"
+}];
 //版本更新信息
 var versions = [
     {
@@ -135,12 +230,12 @@ function initialize(context) {
     var database = context.database;
 
     database.dml().insert("s_menu").values(menus).execute().sync();
-
+    database.dml().insert("s_autz_setting_info").values(autzSettings).execute().sync();
 }
 
 function install(context) {
 
-     
+
 }
 
 
