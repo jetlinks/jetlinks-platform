231ca4728d434003fa05af47d0ac9be279e34bde
liujq <liujq@zmcsoft.com>
2019-11-07 17:16:52 +0800

    修改mqtt客户端状态显示

--- ui/admin/rule-engine/manager/mqttclient/list.js
+++ ui/admin/rule-engine/manager/mqttclient/list.js
@@ -3,13 +3,6 @@ importResource("/admin/css/common.css");
 importMiniui(function () {
     mini.parse();
     require(["request", "miniui-tools", "search-box", "message"], function (request, tools, SearchBox, message) {
-        // var deviceState = [
-        //     {"id": "online", "text": "上线"},
-        //     {"id": "offline", "text": "离线"},
-        //     {"id": "notActive", "text": "未激活"}
-        // ];
-        // var comboboxState = mini.get("_state");
-        // comboboxState.setData(deviceState);
         var tag = request.getParameter("tag");
         new SearchBox({
             container: $("#search-box"),
@@ -34,38 +27,40 @@ importMiniui(function () {
                 grid.reload();
             })
         });
-
+        window.renderStatus = function(e){
+            if (e.value == 1){
+                return "启用";
+            }
+            return "禁用";
+        }
         window.renderAction = function (e) {
             var row = e.record;
             var html = [];
-            if (tag !== 'select'){
-                html.push(tools.createActionButton("编辑", "icon-edit", function () {
-                    tools.openWindow("admin/rule-engine/manager/mqttclient/save.html?id=" + row.id, "编辑mqtt客户端：" + row.name, "80%", "80%", function () {
-                        grid.reload();
-                    });
-                }));
+            html.push(tools.createActionButton("编辑", "icon-edit", function () {
+                tools.openWindow("admin/rule-engine/manager/mqttclient/save.html?id=" + row.id, "编辑mqtt客户端：" + row.name, "80%", "80%", function () {
+                    grid.reload();
+                });
+            }));
 
-                //if (row.state.text === '未激活') {
-                html.push(tools.createActionButton("删除", "icon-remove", function () {
-                    message.confirm("确定删除客户端：" + row.name + "？删除后将无法恢复", function () {
-                        var box = message.loading("删除中...");
-                        request["delete"]("mqtt-client/" + row.id, function (response) {
-                            box.hide();
-                            if (response.status === 200) {
-                                message.showTips("删除成功");
-                                grid.reload();
-                            } else {
-                                message.showTips("删除失败:" + response.message, "danger");
-                            }
-                        });
+            html.push(tools.createActionButton("删除", "icon-remove", function () {
+                message.confirm("确定删除客户端：" + row.name + "？删除后将无法恢复", function () {
+                    var box = message.loading("删除中...");
+                    request["delete"]("mqtt-client/" + row.id, function (response) {
+                        box.hide();
+                        if (response.status === 200) {
+                            message.showTips("删除成功");
+                            grid.reload();
+                        } else {
+                            message.showTips("删除失败:" + response.message, "danger");
+                        }
                     });
-                }));
-            } else {
+                });
+            }));
+            if (tag === 'select'){
                 html.push(tools.createActionButton("选择", "icon-ok", function () {
                     tools.closeWindow(row);
                 }));
             }
-
             return html.join("");
         }
 
