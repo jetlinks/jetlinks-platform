705c56f881558155c3111e78b206ec578747a530
wangzheng <540964791@qq.com>
2019-11-09 16:43:36 +0800

    修改规则引擎短信发送页面

--- ui/admin/rule-engine/model/nodes/sms-sender.js
+++ ui/admin/rule-engine/model/nodes/sms-sender.js
@@ -1,8 +1,26 @@
-define([], function () {
-    
+define(["miniui-tools"], function (tools) {
+
     return {
         init: function (panel, model) {
-            
+            var senderId = mini.getbyName("config.senderId");
+            senderId.on("buttonclick", function () {
+                tools.openWindow("admin/sms/list.html?selector=1", "选择发信人", "600", "400", function (data) {
+                    if (data !== "cancel" || data !== "close") {
+                        senderId.setValue(data.id);
+                        senderId.setText(data.name)
+                    }
+                });
+            });
+
+            var templateId = mini.getbyName("config.templateId");
+            templateId.on("buttonclick", function () {
+                tools.openWindow("admin/sender-template/list.html?selector=1", "选择模板", "600", "400", function (data) {
+                    if (data !== "cancel" || data !== "close") {
+                        templateId.setValue(data.id);
+                        templateId.setText(data.name);
+                    }
+                });
+            });
         },
         debugSupport: true
     }
