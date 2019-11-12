ddb109f3f44b0f058b6e7ae150dd609640f068e4
zhou-hao <admin@hsweb.me>
2019-11-12 11:41:52 +0800

    优化属性同步

--- src/main/java/org/jetlinks/platform/events/handler/DeviceEventMessageHandler.java
+++ src/main/java/org/jetlinks/platform/events/handler/DeviceEventMessageHandler.java
@@ -215,6 +215,7 @@ public class DeviceEventMessageHandler {
                                         });
                                 return entity;
                             })
+                            .peek(processor::onNext)
                             .collect(Collectors.toList());
                    return syncDeviceProperty(entities);
                 })
